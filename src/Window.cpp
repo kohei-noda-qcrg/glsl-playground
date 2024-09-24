458149747489f24be8f9a39313093b27e644b237
Kohei Noda <k.noda@saver.jp>
2024-09-24 16:52:52 +0900

    add Window class

---
+++ src/Window.cpp
@@ -0,0 +1,34 @@
+#include "GL/glew.h"
+
+#include "Window.hpp"
+#include "macros/assert.hpp"
+
+Window::Window(int width, int height, const char* title)
+    : window(glfwCreateWindow(width, height, title, NULL, NULL)) {
+    if(window == NULL) {
+        warn("failed to create GLFW window");
+        exit(1);
+    }
+
+    glfwMakeContextCurrent(window);
+    glewExperimental = GL_TRUE;
+    if(glewInit() != GLEW_OK) {
+        warn("failed to initialize GLEW");
+        exit(1);
+    }
+
+    glfwSwapInterval(1);
+
+    glfwSetWindowSizeCallback(window, resize);
+    resize(window, width, height);
+}
+
+auto Window::swapBuffers() const -> void {
+    glfwSwapBuffers(window);
+}
+
+auto Window::resize(GLFWwindow* window, int width, int height) -> void {
+    int fbwidth, fbheight;
+    glfwGetFramebufferSize(window, &fbwidth, &fbheight);
+    glViewport(0, 0, fbwidth, fbwidth);
+}
---
+++ src/Window.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <GLFW/glfw3.h>
+
+class Window {
+    GLFWwindow* const window;
+
+  public:
+    Window(int width = 640, int height = 480, const char* title = "Hello!");
+
+    virtual ~Window() {
+        glfwDestroyWindow(window);
+    }
+
+    explicit operator bool() const {
+        glfwWaitEvents();
+        return !glfwWindowShouldClose(window);
+    }
+
+    auto swapBuffers() const -> void;
+
+    static auto resize(GLFWwindow* window, int width, int height) -> void;
+};
