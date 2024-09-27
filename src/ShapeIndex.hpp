35a89e55b30ba63d3d8cceded7970e60675946b8
kohei-noda-qcrg <k.noda@saver.jp>
2024-09-27 15:44:53 +0900

    ShapeIndex: remove std::optional from index

--- src/ShapeIndex.hpp
+++ src/ShapeIndex.hpp
@@ -7,18 +7,13 @@
 
 class ShapeIndex : public Shape {
   protected:
-    const std::optional<std::vector<GLuint>> index;
+    const std::vector<GLuint> index;
 
   public:
-    ShapeIndex(const std::vector<Object::Vertex>& vertex, const std::optional<std::vector<GLuint>>& index = std::nullopt)
+    ShapeIndex(const std::vector<Object::Vertex>& vertex, const std::vector<GLuint>& index)
         : Shape(vertex, index), index(index) {}
 
     virtual auto execute() const -> void {
-        if(!index.has_value()) {
-            const auto cur = std::source_location::current();
-            warn(cur.file_name(), " : ", cur.function_name(), " : index has no value, cannot draw elements");
-            return;
-        }
-        glDrawElements(GL_LINES, index->size(), GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_LINES, index.size(), GL_UNSIGNED_INT, 0);
     }
 };
